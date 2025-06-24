3fb83f96c6196a19e1953b063bd5a90c26ddc1ad
LAPTOP-JRUMML2J\ronal <ronaldbejarano147>
2025-06-23 19:35:47 -0500

    atributos de la interfaz

--- src/Interfaz/interfazVehiculo.java
+++ src/Interfaz/interfazVehiculo.java
@@ -6,15 +6,22 @@ import java.awt.*;
 public class InterfazVehiculo extends JFrame {
 	
 	public InterfazVehiculo() {
-        interfaz();
+        ventana();
 
     }
 
-    private void interfaz() {
-        setTitle("Gestión estacionamiento de vehiculos");
+    private void ventana() {
+        setTitle("Gestion estacionamiento de vehiculos");
         setSize(900, 600);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
         setLayout(new BorderLayout(5, 5));
     }
+ private void Interfaz() {
+        
+        PanelListaVehiculos panelLista = new PanelListaVehiculos();
+        PanelDetalleVehiculo panelDetalle = new PanelDetalleVehiculo();
+        PanelOpciones panelOpciones = new PanelOpciones(panelLista, panelDetalle);
+        
+ 	}
 }
