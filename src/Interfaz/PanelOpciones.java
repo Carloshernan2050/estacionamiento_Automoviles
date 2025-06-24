12b088241c7c4c8eda3d94b88d8b1647b0d8d7d2
LAPTOP-JRUMML2J\ronal <ronaldbejarano147>
2025-06-23 22:00:11 -0500

    agregados los botones

--- src/Interfaz/PanelOpciones.java
+++ src/Interfaz/PanelOpciones.java
@@ -14,10 +14,62 @@ public class PanelOpciones extends JPanel {
         this.panelDetalle = panelDetalle;
         
         VentanaOpciones();
+        
     }
     
     private void VentanaOpciones() {
         setLayout(new FlowLayout(FlowLayout.CENTER, 10, 10));
         setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
+    }
+    
+    private void InicializarBotones() {
+    	   
+        JButton btnIngresar = crearBoton("Ingresar Vehículo", "add.png");
+        btnIngresar.addActionListener(this::mostrarDialogoIngreso);
+        
+
+        JButton btnBuscar = crearBoton("Buscar Vehículo", "search.png");
+        btnBuscar.addActionListener(this::mostrarDialogoBusqueda);
+        
+  
+        JButton btnRetirar = crearBoton("Retirar Vehículo", "remove.png");
+        btnRetirar.addActionListener(this::mostrarDialogoRetiro);
+        
+      
+        JButton btnActualizar = crearBoton("Actualizar Lista", "refresh.png");
+        btnActualizar.addActionListener(e -> panelLista.cargarVehiculos());
+        
+  
+        JButton btnSalir = crearBoton("Salir", "exit.png");
+        btnSalir.addActionListener(e -> System.exit(0));
+        
+  
+        add(btnIngresar);
+        add(btnBuscar);
+        add(btnRetirar);
+        add(btnActualizar);
+        add(btnSalir);
+    }
+    
+    private JButton crearBoton(String texto, String icono) {
+        JButton boton = new JButton(texto);
+        boton.setPreferredSize(new Dimension(180, 40));
+        
+        return boton;
+    }
+
+    private void mostrarDialogoIngreso(ActionEvent e) {
+    	VentanaRetiroVehiculo dialogo = new VentanaRetiroVehiculo(panelLista);
+        dialogo.setVisible(true);
+    }
+
+    private void mostrarDialogoBusqueda(ActionEvent e) {
+    	VentanaRetiroVehiculo dialogo = new VentanaRetiroVehiculo();
+        dialogo.setVisible(true);
+    }
+
+    private void mostrarDialogoRetiro(ActionEvent e) {
+        VentanaRetiroVehiculo dialogo = new VentanaRetiroVehiculo(panelLista);
+        dialogo.setVisible(true);
     }
 }
