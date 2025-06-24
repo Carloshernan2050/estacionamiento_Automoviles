02812d76ed76bcbf6fdacde377bd862e8cf0d4c8
LAPTOP-JRUMML2J\ronal <ronaldbejarano147>
2025-06-23 21:28:07 -0500

    busqueda de vehiculo

--- src/Interfaz/VentanaBusquedaVehiculo.java
+++ src/Interfaz/VentanaBusquedaVehiculo.java
@@ -21,6 +21,7 @@ public class VentanaBusquedaVehiculo extends JDialog {
     
     public VentanaBusquedaVehiculo() {
         configurarVentana();
+        inicializarCampos();
     }
 
     private void configurarVentana() {
@@ -31,7 +32,7 @@ public class VentanaBusquedaVehiculo extends JDialog {
         setLayout(new BorderLayout(5, 5));
     }
     
-private void inicializarComponentes() {
+private void inicializarCampos() {
         
         JPanel panelBusqueda = new JPanel(new FlowLayout(FlowLayout.CENTER, 10, 10));
         panelBusqueda.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
@@ -76,4 +77,47 @@ private void inicializarComponentes() {
         add(scrollResultado, BorderLayout.CENTER);
         add(panelBotones, BorderLayout.SOUTH);
     }
+	private void buscarVehiculo(String placa, JTextArea txtResultado) throws SQLException {
+	    StringBuilder sb = new StringBuilder();
+	    
+	    VehiculoDAO vDao = new VehiculoDAO();
+	    Vehiculo vehiculo = vDao.buscarVehiculoPorPlaca(placa);
+	    
+	    if (vehiculo == null) {
+	        sb.append("No se encontró vehículo con placa: ").append(placa);
+	    } else {
+	        sb.append("informacion del vehiculo\n");
+	        sb.append(String.format("%-12s: %s%n", "Placa", vehiculo.getPlaca()));
+	        sb.append(String.format("%-12s: %s%n", "Marca", vehiculo.getMarca()));
+	        sb.append(String.format("%-12s: %s%n", "Modelo", vehiculo.getModelo()));
+	        sb.append(String.format("%-12s: %s%n", "Propietario", vehiculo.getPropietario()));
+	        sb.append("\n");
+	        
+
+	        EstacionamientoDAO eDao = new EstacionamientoDAO();
+	        Estacionamiento estacionamiento = eDao.buscarEstacionamientoPorPlaca(placa);
+	        
+	        if (estacionamiento != null) {
+	            sb.append("=== INFORMACIÓN DE ESTACIONAMIENTO ===\n");
+	            sb.append(String.format("%-12s: %d%n", "Lugar", estacionamiento.getNumParqueo()));
+	            sb.append(String.format("%-12s: %s%n", "Ingreso", 
+	                estacionamiento.getFechaIngreso().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm"))));
+	            
+	            if (estacionamiento.getFechaRetiro() != null) {
+	                sb.append(String.format("%-12s: %s%n", "Retiro", 
+	                    estacionamiento.getFechaRetiro().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm"))));
+	                
+	                long horas = java.time.Duration.between(
+	                    estacionamiento.getFechaIngreso(), estacionamiento.getFechaRetiro()).toHours();
+	                sb.append(String.format("%-12s: %d horas%n", "Duración", horas));
+	            } else {
+	                sb.append(String.format("%-12s: %s%n", "Estado", "EN ESTACIONAMIENTO"));
+	            }
+	        } else {
+	            sb.append("No hay información de estacionamiento para este vehículo");
+	        }
+	    }
+	    
+	    txtResultado.setText(sb.toString());
+	}
 }
