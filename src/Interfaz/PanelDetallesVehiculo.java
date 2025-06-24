3746b33407b02c068b962ae5112daeb8ce9abf39
LAPTOP-JRUMML2J\ronal <ronaldbejarano147>
2025-06-23 20:34:32 -0500

    agregados los atributos y la interfaz

--- src/Interfaz/PanelDetallesVehiculo.java
+++ src/Interfaz/PanelDetallesVehiculo.java
@@ -13,12 +13,52 @@ import mundo.*;
 
 public class PanelDetallesVehiculo extends JPanel {
 	
+	private JTextArea txtDetalle;
+	
+    private JLabel lblImagen;
+    
+    
 	 public PanelDetallesVehiculo() {
 	        detallesVehiculo();
+	        interfaz();
 	    }
 
 	    private void detallesVehiculo() {
 	        setLayout(new BorderLayout(10, 10));
 	        setBorder(BorderFactory.createTitledBorder("Detalles del Vehículo"));
+	    }
+	    
+	    private void interfaz() {
+	        
+	        JPanel panelImagen = new JPanel(new BorderLayout());
+	        panelImagen.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+	        lblImagen = new JLabel(new ImageIcon(ajustarImagen("imagenes/vehiculo.png", 200, 150)));
+	        lblImagen.setHorizontalAlignment(JLabel.CENTER);
+	        panelImagen.add(lblImagen, BorderLayout.CENTER);
+
+	        
+	        JPanel panelTexto = new JPanel(new BorderLayout());
+	        txtDetalle = new JTextArea(10, 30);
+	        txtDetalle.setEditable(false);
+	        txtDetalle.setLineWrap(true);
+	        txtDetalle.setWrapStyleWord(true);
+	        txtDetalle.setBackground(new Color(240, 240, 240));
+	        txtDetalle.setFont(new Font("Monospaced", Font.PLAIN, 14));
+	        
+	        JScrollPane scrollDetalle = new JScrollPane(txtDetalle);
+	        panelTexto.add(scrollDetalle, BorderLayout.CENTER);
+
+	        
+	        add(panelImagen, BorderLayout.WEST);
+	        add(panelTexto, BorderLayout.CENTER);
+	    }
+	    
+	    private Image ajustarImagen(String ruta, int ancho, int alto) {
+	        try {
+	            BufferedImage img = ImageIO.read(new File(ruta));
+	            return img.getScaledInstance(ancho, alto, Image.SCALE_SMOOTH);
+	        } catch (IOException e) {
+	            return new BufferedImage(ancho, alto, BufferedImage.TYPE_INT_ARGB);
+	        }
 	    }
 }
